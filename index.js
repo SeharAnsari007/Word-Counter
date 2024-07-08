1c00a1ce336e3ca358bdf9e80565e4b28b9f7ac9
SeharAnsari007 <seharansari007@gmail.com>
2024-07-08 01:40:22 -0700

    Adding Word Cunter

---
+++ index.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+console.log(chalk.bold.bgBlueBright("\n\tWelcome to code with Sehar - Words Counter\n"));
+console.log("=".repeat(60));
+let answer = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter a sentence..."
+    }
+]);
+let words = answer.Sentence.trim().split(" ");
+console.log("=".repeat(60));
+console.log("Sentence words:");
+console.log(words);
+console.log(chalk.bold.bgCyan(`\n\tWord Count: ${words.length}`));
+console.log("=".repeat(60));
